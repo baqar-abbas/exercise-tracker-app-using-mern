691564bf5143d2d3b4f4895454ccdcfe9731b479
baqar-abbas <baqar007@hotmail.com>
2023-12-06 04:36:26 +0500

    create navbar and basic components for the front end react app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-// import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/navbar.component.js";
+import ExercisesList from "./components/exercises-list.component.js";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 // import './App.css';
 
 function App() {
   return (
-    <div className="Container">
-      Hello World
+    <Router>
+      
+    <div className="container">
+    <Navbar />
+    <br/>
+     <Routes>
+      {/* <Route path="/" component={<ExercisesList/>} />
+      <Route path="/edit/:id" component={<EditExercise/>} />
+      <Route path="/create" component={<CreateExercise/>} />
+      <Route path="/user" component={<CreateUser/>} /> */}
+      <Route path="/" element={<ExercisesList />} />
+          <Route path="/edit/:id" element={<EditExercise />} />
+          <Route path="/create" element={<CreateExercise />} />
+          <Route path="/user" element={<CreateUser />} />
+      </Routes>
     </div>
+    </Router>
   );
 }
 
